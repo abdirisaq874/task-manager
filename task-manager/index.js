c2ced4fad4a88b74635f01d3da0e00eb8808d356
mandeez <abdirisaq874@gmail.com>
2022-06-24 12:38:38 +0300

    task-manager

--- task-manager/index.js
+++ task-manager/index.js
@@ -1,7 +1,6 @@
 const express = require("express")
 const app = express()
 const db = require("./db/oracledb.js")
-const forecast = require('./../web-server/src/utils/forecast')
 
 
 const port =process.env.PORT || 4100
